5446f71f0cf9d02315ad5b639027e88f8f82ff20
Glauber Brack <glauber@brack.com.br>
2020-07-18 11:47:53 -0300

    reseting global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+
+import GlobalStyles from './styles/global';
 
 import Routes from './routes';
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <>
+      <GlobalStyles />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #f3f4f6;
+    width: 1366px;
+    height: 970px;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Inter';
+    font-size: 10px;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
