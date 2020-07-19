13cad81886093fe2d80dce7b5089779626ae8f5c
Glauber Brack <glauber@brack.com.br>
2020-07-19 16:20:23 -0300

    using api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,2 +1,5 @@
 import axios from 'axios';
 import { ICar } from '../utils/interfaces/ICar';
+
+export const getAllVehicles = () =>
+  axios.get<ICar[]>('http://www.mocky.io/v2/5eb553df31000060006994a8');
