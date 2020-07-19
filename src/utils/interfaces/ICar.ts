f013e5ca2056651f80aec4dfab97f65bc4bf611b
Glauber Brack <glauber@brack.com.br>
2020-07-19 16:17:29 -0300

    creating car interface

---
+++ src/utils/interfaces/ICar.ts
@@ -0,0 +1,18 @@
+export interface ICar {
+  evaluationType: string;
+  versionUuid: string;
+  mileage: number;
+  modelYear: number;
+  name: string;
+  transmissionType: string;
+  fuelType: string;
+  vehicleUuid: string;
+  modelName: string;
+  manufacturingYear: number;
+  versionName: string;
+  image: string;
+  brandName: string;
+  adSellingPrice: number;
+  modelUuid: string;
+  brandUuid: string;
+}
